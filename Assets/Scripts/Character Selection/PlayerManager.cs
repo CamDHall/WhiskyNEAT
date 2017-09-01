4ac7fcba029e0e7badebfde417104d2740ecc5ed
CamDHall <camerondylan.h@gmail.com>
2017-08-31 20:17:34 -0400

    Follower cards

--- Assets/Scripts/Character Selection/PlayerManager.cs
+++ Assets/Scripts/Character Selection/PlayerManager.cs
@@ -7,8 +7,14 @@ public class PlayerManager : MonoBehaviour {
 
     public GameObject[] heroList = new GameObject[4];
     GameObject[] _heros = new GameObject[4];
+    GameObject[] deck1, deck2;
     public int selectedHero = 0, player = 0;
     Vector3 heroPos;
+
+    public PlayerInfo playerInfo;
+
+    public GameObject heroSelector, followerSelector;
+    public Text followerStatPrefab;
 
     // UI Compents
     public Text stats;
@@ -30,7 +36,27 @@ public class PlayerManager : MonoBehaviour {
                 _heros[i].SetActive(false);
             else
                 _heros[i].SetActive(true);
-        } 
+        }
+
+        deck1 = new GameObject[playerInfo.deckPlayer1.Count];
+        deck2 = new GameObject[playerInfo.deckPlayer2.Count];
+
+        // Spawn deck inactive
+        for(int i = 0; i < deck1.Length; i++)
+        {
+            Vector3 Pos = new Vector3(-6 + (i * 3), 4, 0);
+            var card = Instantiate(playerInfo.deckPlayer1[i], Pos, playerInfo.deckPlayer1[i].transform.rotation);
+            deck1[i] = card;
+            deck1[i].SetActive(false);
+        }
+
+        for(int i = 0; i < deck2.Length; i++)
+        {
+            Vector3 Pos = new Vector3(-6 + (i * 3), -4, 0);
+            var card = Instantiate(playerInfo.deckPlayer2[i], Pos, playerInfo.deckPlayer2[i].transform.rotation);
+            deck2[i] = card;
+            deck2[i].SetActive(false);
+        }
 
         selectedHero = 0;
         DisplayStats(_heros[selectedHero].GetComponent<CharacterData>());
@@ -94,5 +120,36 @@ public class PlayerManager : MonoBehaviour {
     {
         stats.text = data.characterName + "\nHealth: \t" + data.health + "\nCourage: \t" + data.courage + "\nMoves: \t" + data.moves + "\nMelee Strength: \t" + data.meleeStrength
             + "\nRanged Strength: \t" + data.rangedStrength;
+    }
+
+    public void ConfirmHero()
+    {
+        // Set heros and turn them off
+        if(player == 1)
+        {
+            playerInfo.heroPlayer1 = selectedHero;
+            // Display deck
+            for(int i = 0; i < deck1.Length; i++)
+            {
+                deck1[i].SetActive(true);
+                Vector3 statPos = new Vector3(-515 + (i * 275), 0, 0);
+                Text stat = Instantiate(followerStatPrefab, Vector3.zero, Quaternion.identity, followerSelector.transform);
+                stat.GetComponent<RectTransform>().anchoredPosition = statPos;
+                CharacterData data = deck1[i].GetComponent<CharacterData>();
+                stat.text = data.characterName + "\nHealth: \t" + data.health + "\nCourage: \t" + data.courage + "\nMoves: \t" + data.moves + "\nMelee Strength: \t" + data.meleeStrength
+            + "\nRanged Strength: \t" + data.rangedStrength;
+            }
+        } else
+        {
+            playerInfo.heroPlayer2 = selectedHero;
+            foreach (GameObject card in deck1)
+            {
+                card.SetActive(true);
+            }
+        }
+
+        heroSelector.SetActive(false);
+        _heros[selectedHero].SetActive(false);
+
     }
 }
---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour {
+
+    public int heroPlayer1, heroPlayer2;
+    public List<GameObject> deckPlayer1, deckPlayer2;
+}
